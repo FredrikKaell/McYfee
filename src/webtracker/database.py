071cf870cb82afdcf82f697b8032ad099016587d
emilioano <emil.sjoekvist@gmail.com>
2026-02-03 18:02:20 +0100

    DB actions created

---
+++ src/webtracker/database.py
@@ -0,0 +1,283 @@
+import mysql.connector
+from mysql.connector import Error
+import json
+from datetime import datetime
+
+from config import DB_CONFIG
+
+
+''' Create connection '''
+
+def db_connection():
+    # Connect to DB
+    try:
+        conn = mysql.connector.connect(**DB_CONFIG)
+        return conn
+    except Error as err:
+        print(f"Error connecting to database: {err}")
+        raise
+
+
+
+''' DB Read actions '''
+
+def fetch_monitors(active: bool = True):
+    conn = db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    try:
+        query = 'SELECT * FROM monitors WHERE is_active = %s'
+
+        cursor.execute(query, (1 if active else 0,))
+        result = cursor.fetchall()
+        return result
+
+    except Exception as err:
+        print(f'Error: {err}')   
+        return None 
+
+    finally:
+        cursor.close()
+        conn.close()  
+
+
+def fetch_all_monitors():
+    conn = db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    try:
+        query = 'SELECT * FROM monitors'
+
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+
+    except Exception as err:
+        print(f'Error: {err}') 
+        return None    
+
+    finally:
+        cursor.close()
+        conn.close()      
+
+
+def fetch_selectors():
+    conn = db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    try:
+        query = 'SELECT * FROM selectors'
+
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+
+    except Exception as err:
+        print(f'Error: {err}')  
+        return None  
+
+    finally:
+        cursor.close()
+        conn.close()  
+
+
+def fetch_notifications(active: bool = True):
+    conn = db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    try:
+        query = 'SELECT * FROM notifications WHERE active = %s'
+
+        cursor.execute(query, (1 if active else 0,))
+        result = cursor.fetchall()
+        return result
+
+    except Exception as err:
+        print(f'Error: {err}') 
+        return None   
+
+    finally:
+        cursor.close()
+        conn.close()  
+
+
+def fetch_monitors_poller(type: str = 'all_active'):
+    # Fetching all monitors in active state that has connected selector and notification
+    # With option 'all_due' we're only showing monitors that are ready to be triggered!
+    conn = db_connection()
+    cursor = conn.cursor(dictionary=True)
+    
+    try: 
+        query = '''
+            SELECT
+                m.id,
+                m.name,
+                m.url,
+                m.selector_id,
+                m.check_interval,
+                m.type,
+                m.threshold_value,
+                m.is_active,
+                m.last_check_at,
+                m.created_at,
+                s.name as selector_name,
+                s.css_selector,
+                s.description,
+                m.notification_id,
+                n.type as notification_type,
+                n.config as notification_config
+                FROM monitors AS m
+                INNER JOIN selectors as s on s.id = m.selector_id
+                INNER JOIN notifications as n on n.id = m.notification_id
+                WHERE is_active = 1
+        '''
+
+        if type == 'all_due':  
+            query += '''
+                    AND (
+                    m.last_check_at IS NULL 
+                    OR TIMESTAMPDIFF(MINUTE , m.last_check_at, NOW()) >= m.check_interval
+                    )
+            '''
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+
+    except Exception as err:
+        print(f'Error: {err}')   
+        return None 
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+
+''' Insert, update, delete actions '''
+
+def create_monitor(name: str, url: str, selector_id: int, type: str, threshold: int, check_interval: int, is_active: int):
+    # Create a new monitor
+    conn = db_connection()
+    cursor = conn.cursor()
+    
+    try: 
+        query = '''
+            INSERT INTO 
+            monitors (name, url, selector_id, type, threshold_value, check_interval, is_active) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        '''
+        cursor.execute(query, (name, url, selector_id, type, threshold, check_interval, is_active))
+        conn.commit()
+        return cursor.rowcount
+
+    except Exception as err:
+        print(f'Error: {err}')   
+        return None 
+
+    finally:
+        cursor.close()
+        conn.close()
+
+def set_monitor_status(id: int= None, status: int = 0):
+    # Upate status on monitor
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    try:
+        query = 'UPDATE monitors SET is_active = %s where id = %s'
+        cursor.execute(query, (status, id))
+        conn.commit()
+        return cursor.rowcount
+
+    except Exception as err:
+        print(f'Error: {err}')   
+        return None 
+
+    finally:
+        cursor.close()
+        conn.close()
+
+def set_notification_status(id: int= None, status: int = 0):
+    # Upate status on notification
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    try:
+        query = 'UPDATE notifications SET is_active = %s where id = %s'
+        cursor.execute(query, (status, id))
+        conn.commit()
+        return cursor.rowcount
+
+    except Exception as err:
+        print(f'Error: {err}')   
+        return None 
+
+    finally:
+        cursor.close()
+        conn.close()
+
+def delete_monitor(id: int = None):
+    # Delete a monitor
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    try:
+        query = 'DELETE FROM monitors where id = %s'
+        cursor.execute(query, (id,))
+        conn.commit()
+        return cursor.rowcount
+
+    except Exception as err:
+        print(f'Error: {err}')    
+        return None
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+
+''' DB audit field updates '''
+
+def update_monitor_last_check(id: int = None, timestamp: datetime = None):
+    # Upate monitor set last checked
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    try:
+        if timestamp == None:
+            timestamp = datetime.now()
+        query = 'UPDATE monitors SET last_check_at = %s where id = %s'
+        cursor.execute(query, (timestamp, id))
+        conn.commit()
+        return cursor.rowcount
+
+    except Exception as err:
+        print(f'Error: {err}')    
+        return None
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+
+if __name__ == '__main__':
+    #Tester
+    db_connection()
+
+    print('fetch_all_monitors():')
+    for row in fetch_monitors():
+        print(f'{row}\n')
+    
+    print('fetch_monitors():')
+    for row in fetch_monitors():
+        print(f'{row}\n')
+
+    print('fetch_selectors():')
+    for row in fetch_selectors():
+        print(f'{row}\n')
+
+    print('fetch_notifications():')
+    for row in fetch_selectors():
+        print(f'{row}\n')
---
+++ src/webtracker/tracker.py
@@ -0,0 +1,107 @@
+import time
+import datetime
+from datetime import timedelta
+
+import database as db
+
+
+POLL_RATE = 10
+
+
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def tracker():
+    while True:
+
+        date_time = datetime.datetime.now()
+
+        for row in db.fetch_monitors_poller('all_due'): 
+
+            def scraper_function():
+                print(colors.OKGREEN)
+                print('Scraper function triggered!!')
+                print(colors.ENDC)
+
+            def messageout():
+                print('Messageout function tiggered!!')
+
+
+            if (row.get('is_active')) == 1:
+                monitor_id = row.get('id')
+                monitor_name = row.get('name')
+                monitor_url = row.get('url')
+                monitor_type = row.get('type')
+                monitor_threshold = row.get('threshold_value')
+
+                selector_id = row.get('selector_id')
+                selector_name = row.get('selector_name')
+                selector_description = row.get('description')
+                selector_css = row.get('css_selector')
+
+                notification_type = row.get('notification_type')
+                notification_config = row.get('notification_config')
+
+                check_interval = row.get('check_interval')
+                last_checked = row.get('last_check_at')
+
+                created_at = row.get('created_at')
+
+                if last_checked:
+                    next_check = last_checked + timedelta(minutes=check_interval)
+                else:
+                    next_check = created_at + timedelta(minutes=check_interval)
+
+                print('='*60)
+                print(f'Monitor id: {monitor_id}')
+                print(f'Monitor name: {monitor_name}')
+                print(f'Monitor url: {monitor_url}')
+                print(f'Monitor threshold: {monitor_threshold}')
+
+                print()
+
+                print(f'Selector name: {selector_name}')
+                print(f'Selector description: {selector_description}')
+                print(f'Selector css: {selector_css[:150]}...')
+                print(f'Notification via: {notification_type}')
+
+                print()
+
+                print(f'Check interval: {check_interval} minutes')
+
+                print(f'Created at: {created_at}')
+                print(f'Last check at: {last_checked}')
+                print(f'Next check at: {next_check}')
+                print(f'Now time: {date_time}')
+                
+
+                if next_check <= date_time:
+                    print(colors.OKCYAN)
+                    print('Trigger hit!!')
+                    print(colors.ENDC)
+
+                    scraper_function()
+
+                    update_last_check = db.update_monitor_last_check(monitor_id)
+                    print(f'{update_last_check} row for last_check updated in db')
+
+                print('='*60)
+                print('\n')
+        print(colors.OKBLUE)
+        print(f'Chillar i {POLL_RATE} sekunder.')
+        print(colors.ENDC)
+        time.sleep(POLL_RATE)
+
+
+
+
+if __name__ == '__main__':
+    tracker()

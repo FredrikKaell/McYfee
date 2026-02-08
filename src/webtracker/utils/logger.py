109eb6a7556549f0235891109e69fc8000468f37
emilioano <emil.sjoekvist@gmail.com>
2026-02-08 15:05:24 +0100

    Path to project root

--- src/webtracker/utils/logger.py
+++ src/webtracker/utils/logger.py
@@ -4,12 +4,14 @@
 
 import logging
 from pathlib import Path
+from webtracker.config import PROJECT_ROOT
+
 class AppLogger:
     def __init__(self, name: str = "mcyfee", log_level: int = logging.INFO):
         self.name = name
         self.log_level = log_level
 
-        self.log_dir = Path("logs")
+        self.log_dir = PROJECT_ROOT / 'logs'
         self.log_file = self.log_dir / "app.log"
 
         self._ensure_log_directory()

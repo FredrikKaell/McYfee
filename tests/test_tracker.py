0d349f3098c8dda93fdcd65a310c7c5a1a861c3c
emilioano <emil.sjoekvist@gmail.com>
2026-02-11 19:45:43 +0100

    pytest: Minor bugfix in test_tracker

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,5 +1,5 @@
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from webtracker.core.tracker import worker_function
 
 
@@ -20,31 +20,32 @@ MOCK_MONITOR_ROW = {
 
 
 @patch('webtracker.utils.performance.db.save_performance_record') # Don't save real performance record
-@patch('webtracker.core.tracker.send_notification')   # Do not send real notification
+@patch('webtracker.notifications.discord_notifier.DiscordNotifier')   # Do not send real notification
 @patch('webtracker.scraper.parser.parse')               # Don't fetch real webpage
 @patch('webtracker.database.database')                  # Do not not create db record
-def test_full_flow_price_below_threshold(mock_db, mock_parse, mock_notification, mock_perf):
+def test_full_flow_price_below_threshold(mock_db, mock_parse, mock_discord, mock_perf):
     # Test flow. Parse > Extract price > Compare > Trigger
     mock_parse.return_value = "34995 kr"
     mock_db.update_monitor_last_check.return_value = True
     mock_db.create_snapshot.return_value = 1
     mock_db.update_monitor_values.return_value = True
+    mock_db.set_monitor_status.return_value = 1
+
+    mock_discord_instance = MagicMock()
+    mock_discord.return_value = mock_discord_instance
 
     worker_function(MOCK_MONITOR_ROW)
 
     # Notification should trigger. Under 40000
-    mock_notification.assert_called_once()
+    mock_discord.assert_called_once()
+    mock_discord_instance.send.assert_called_once()
 
 @patch('webtracker.utils.performance.db.save_performance_record')
-@patch('webtracker.core.tracker.send_notification')
+@patch('webtracker.notifications.discord_notifier.DiscordNotifier')
 @patch('webtracker.scraper.parser.parse')
 @patch('webtracker.database.database')
-def test_full_flow_price_above_threshold(mock_db, mock_parse, mock_notification, mock_perf):
-    """
-    Test full flow:
-    parse → extract price → compare → no notification
-    """
-    # Simulera att sidan returnerar ett pris över threshold
+def test_full_flow_price_above_threshold(mock_db, mock_parse, mock_discord, mock_perf):
+    # Simulate return value above threshold
     mock_parse.return_value = "44995 kr"
     mock_db.update_monitor_last_check.return_value = True
     mock_db.create_snapshot.return_value = 1
@@ -53,5 +54,5 @@ def test_full_flow_price_above_threshold(mock_db, mock_parse, mock_notification,
     worker_function(MOCK_MONITOR_ROW)
 
     # Notification should not send, over 40000
-    mock_notification.assert_not_called()
+    mock_discord.assert_not_called()
 
